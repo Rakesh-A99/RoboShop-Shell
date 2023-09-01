dcacfad4dd82510eabf01d560da979da134b6b25
rakesh <rakeshavulapati1999@gmail.com>
2023-09-01 13:06:13 +0530

    create servers script added

--- create_servers.sh
+++ create_servers.sh
@@ -5,9 +5,8 @@ IMAGE_ID=ami-03265a0778a880afb
 SECUIRTY_GROUP_ID=sg-0fd38dba987f6a767
 DOMAIN_NAME=myroboshop.site
 HOSTED_ZONE=Z08892663AT899M4JPPZH
-
 for i in $@
-do
+do  
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
@@ -17,16 +16,17 @@ do
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch'
+
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
-        "Comment": "CREATE a record ",
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
-                                    "Name": "'$i.$DOMAIN_NAME'",
-                                    "Type": "A",
-                                    "TTL": 300,
-                                    "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                                    }}]
-    }'
- done
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+done
