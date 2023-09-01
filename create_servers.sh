af6898b00c8f9f12185f77ec6ad9793de1919461
rakesh <rakeshavulapati1999@gmail.com>
2023-09-01 17:42:18 +0530

    server creation script

--- create_servers.sh
+++ create_servers.sh
@@ -8,31 +8,15 @@ HOSTED_ZONE=Z08892663AT899M4JPPZH
 
 for i in $@
 do  
-    INSTANCE_EXISTS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$i" --query 'Reservations[].Instances[0].InstanceId' --output text)
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    if [ -n "$instance_exists" ]; 
-    then
-    echo "Instance already exists with ID: $instance_exists"
-    else
-     # Create a new EC2 instance
-    echo "Creating a new EC2 instance..."
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECUIRTY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
-    fi
-    dns_info=$(aws route53 list-resource-record-sets --hosted-zone-id "${HOSTED_ZONE}")
-
-    # Extract the DNS name if it exists
-    existing_dns_name=$(echo "$dns_info" | jq -r '.ResourceRecordSets[] | select(.Name == "'${DOMAIN_NAME}'") | .Name')
-
-    if [ -n "$existing_dns_name" ]; then
-    echo "DNS name already exists: $existing_dns_name"
-    else
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch '
     {
             "Changes": [{
@@ -45,5 +29,4 @@ do
                         }}]
     }
     '
-    fi
 done
