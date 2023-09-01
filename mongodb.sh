e08cc335dc5533a905c47330d0d77a581f91a820
rakesh <rakeshavulapati1999@gmail.com>
2023-09-01 19:06:17 +0530

    mongo script added

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "enabling mongod"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "starting mongod"
+
+sed 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "editting /etc/mongod.conf"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "restarting mongod"
+
