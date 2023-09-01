0fba3ac7b0b532c350ace089bc072fdc2e79d10b
rakesh <rakeshavulapati1999@gmail.com>
2023-09-01 23:00:22 +0530

    catalogue script added

--- user.sh
+++ user.sh
@@ -9,7 +9,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-if [ $USERID -ne 0]; then
+if [$USERID -ne 0]; then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
