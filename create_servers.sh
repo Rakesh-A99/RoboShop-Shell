f5cf0a95dc6459b4b4864466245d98c511684c74
rakesh <rakeshavulapati1999@gmail.com>
2023-09-01 13:03:18 +0530

    create servers script added

--- create_servers.sh
+++ create_servers.sh
@@ -8,6 +8,12 @@ HOSTED_ZONE=Z08892663AT899M4JPPZH
 
 for i in $@
 do
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
