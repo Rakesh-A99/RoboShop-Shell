ef4f4c02f0ec2a58d47aac3fb3a1b656365a73f6
rakesh <rakeshavulapati1999@gmail.com>
2023-09-08 14:57:38 +0530

    servers creation list

--- create-ec2servers.sh
+++ create-ec2servers.sh
@@ -13,26 +13,34 @@ HOSTEDZONE_ID="Z08892663AT899M4JPPZH"
 # Loop through instance names provided as arguments
 for instance_name in "$@"
 do
-    echo "Creating $instance_name instance"
+    echo "Checking if $instance_name instance already exists..."
 
-    # Create the EC2 instance and capture the Private IP address
-    IP_ADDRESS=$(aws ec2 run-instances \
-        --image-id $AMI_ID \
-        --instance-type $INSTANCE_TYPE \
-        --security-group-ids $AWS_SG \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" | jq -r '.Instances[0].PrivateIpAddress')
+    # Use describe-instances to check if an instance with the same name tag exists
+    existing_instance=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$instance_name")
 
-    if [ -n "$IP_ADDRESS" ]; then
-        echo "Created $instance_name instance with Private IP: $IP_ADDRESS"
+    if [ -z "$existing_instance" ]; then
+        echo "$instance_name instance does not exist. Creating..."
 
-        # Optionally, add more configuration steps here if needed
+        # Create the EC2 instance and capture the Private IP address
+        IP_ADDRESS=$(aws ec2 run-instances \
+            --image-id $AMI_ID \
+            --instance-type $INSTANCE_TYPE \
+            --security-group-ids $AWS_SG \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" | jq -r '.Instances[0].PrivateIpAddress')
 
+        if [ -n "$IP_ADDRESS" ]; then
+            echo "Created $instance_name instance with Private IP: $IP_ADDRESS"
+
+            # Optionally, add more configuration steps here if needed
+
+        else
+            echo "Failed to create $instance_name instance"
+        fi
     else
-        echo "Failed to create $instance_name instance"
+        echo "$instance_name instance already exists. Skipping..."
     fi
 
     # Optionally, associate the instance with a Route 53 hosted zone
     # aws route53 change-resource-record-sets ...
 
 done
-          
